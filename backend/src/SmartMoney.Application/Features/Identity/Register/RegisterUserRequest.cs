489cdbdc3c09a7a0c5c6c71df68dfdd5928602ed
Tushar Roy <roytushar55@gmail.com>
2026-07-09 18:59:32 +0530

    feat(identity): add register feature skeleton

---
+++ backend/src/SmartMoney.Application/Features/Identity/Register/RegisterUserRequest.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SmartMoney.Application.Features.Identity.Register
+{
+    public class RegisterUserRequest
+    {
+    }
+}
