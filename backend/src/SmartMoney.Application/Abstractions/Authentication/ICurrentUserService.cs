84c8287e827f5e867c74db8a98fb90c2ce0ef157
Tushar Roy <roytushar55@gmail.com>
2026-07-09 18:31:12 +0530

    feat(application): add authentication and persistence contracts

---
+++ backend/src/SmartMoney.Application/Abstractions/Authentication/ICurrentUserService.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SmartMoney.Application.Abstractions.Authentication
+{
+    internal class ICurrentUserService
+    {
+    }
+}
---
+++ backend/src/SmartMoney.Application/Abstractions/Persistence/IRoleRepository.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SmartMoney.Application.Abstractions.Persistence
+{
+    internal class IRoleRepository
+    {
+    }
+}
